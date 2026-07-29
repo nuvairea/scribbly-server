ab447375900291a4ee489993b1ff20e71d5962a6
paulspective <nuvairea@gmail.com>
2026-07-29 14:56:55 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('./models/User.js');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection failed', err));
+
+app.post('/signup', async (req, res) => {
+  try {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({error: 'Email and password are required'});
+  }
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser){
+    return res.status(409).json({error: 'An account with this email already exist'});
+  }
+
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = await User.create({ email, passwordHash });
+
+  res.status(201).json({ message: 'Account created', userId: user._id });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
