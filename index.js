acbd32a93d8f1b5b1f5061cfc8899b13589244a9
paulspective <nuvairea@gmail.com>
2026-08-02 23:46:46 +0100

    configure persistent sessions

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 } // 7 days
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 }, // 30 days
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: 'lax',
 }));
 
 mongoose.connect(process.env.MONGODB_URI)
